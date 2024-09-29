789eb02e1a43f8b9028552555dda64d1ca13dc48
ABarsen <kiplagatemmanuel21@gmail.com>
2024-09-29 22:54:31 +0300

    initial commit

---
+++ evo/src/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+   <nav>
+    <span className="logo">TESLA</span>
+    <li>
+        <ul>Vehicles</ul>
+        <ul>Energy</ul>
+        <ul> charging</ul>
+        <ul>discover</ul>
+        <ul>shop</ul>
+    </li>
+   </nav>
+  )
+}
+
+export default Navbar
---
+++ evo/src/Page1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import tesla-model-y-website from './assets/tesla-model-y-website.jpg'
+
+const Page1 = () => {
+  return (
+    <h1>Model Y</h1>
+    <p>$0 Due at signing</p>
+    from $34,950
+  )
+}
+
+export default Page1

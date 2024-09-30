24e9d50258fac0b845bfda87026c8818d3d6671f
Barsende <kiplagatemmanuel21@gmail.com>
2024-09-30 17:20:54 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar.jsx';
+import Page1 from './Page1.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    <div className='page1'>
+      <page1/>
+    </div>
+    </div>
+    
+  );
+}
+
+export default App 
---
+++ src/Page1.jsx
@@ -0,0 +1,12 @@
+
+import "./Page1.css";
+import "./assets/tesla-model-y-website";
+const Page1 = () => {
+    const 
+  return (
+    <h1>Model Y</h1>
+
+  )
+}
+
+export default Page1
